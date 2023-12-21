8097407139e13e8eb3fd60dcd5a6651292ab3f61
meiyoleg  git config --global user.email lonelinessuck@mail.ru  git config --global user.name meiyoleg <lonelinessuck@mail.ru>
2023-12-21 19:13:51 +0300

    base

---
+++ script.js
@@ -0,0 +1,55 @@
+function numberZero() {
+    let a = document.getElementById("results").value = +0;
+    document.getElementById("results").innerHTML = a;
+}
+
+function numberOne() {
+    let a = document.getElementById("results").value = +1;
+    document.getElementById("results").innerHTML = a;
+}
+
+function numberTwo() {
+    let a = document.getElementById("results").value = +2;
+    document.getElementById("results").innerHTML = a;
+}
+
+function numberThree() {
+    let a = document.getElementById("results").value = +3;
+    document.getElementById("results").innerHTML = a;
+}
+
+function numberFour() {
+    let a = document.getElementById("results").value = +4;
+    document.getElementById("results").innerHTML = a;
+}
+
+function numberFive() {
+    let a = document.getElementById("results").value = +5;
+    document.getElementById("results").innerHTML = a;
+}
+
+function numberSix() {
+    let a = document.getElementById("results").value = +6;
+    document.getElementById("results").innerHTML = a;
+}
+
+function numberSeven() {
+    let a = document.getElementById("results").value = +7;
+    document.getElementById("results").innerHTML = a;
+}
+
+function numberEight() {
+    let a = document.getElementById("results").value = +8;
+    document.getElementById("results").innerHTML = a;
+}
+
+function numberNine() {
+    let a = document.getElementById("results").value = +9;
+    document.getElementById("results").innerHTML = a;
+}
+
+function clearButton() {
+    let a = document.getElementById("results").value = 0;
+    document.getElementById("results").innerHTML = a;
+}
+
