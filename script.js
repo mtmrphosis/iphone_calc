e09caa2fefc8ce2f431f0e26a30a045096b32f2b
meiyoleg  git config --global user.email lonelinessuck@mail.ru  git config --global user.name meiyoleg <lonelinessuck@mail.ru>
2023-12-23 18:44:07 +0300

    fixed numbers

--- script.js
+++ script.js
@@ -1,55 +1,146 @@
+
 function numberZero() {
-    let a = document.getElementById("results").value = +0;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +0;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +0;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function numberOne() {
-    let a = document.getElementById("results").value = +1;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +1;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +1;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function numberTwo() {
-    let a = document.getElementById("results").value = +2;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +2;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +2;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function numberThree() {
-    let a = document.getElementById("results").value = +3;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +3;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +3;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function numberFour() {
-    let a = document.getElementById("results").value = +4;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +4;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +4;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function numberFive() {
-    let a = document.getElementById("results").value = +5;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +5;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +5;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function numberSix() {
-    let a = document.getElementById("results").value = +6;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +6;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +6;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function numberSeven() {
-    let a = document.getElementById("results").value = +7;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +7;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +7;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function numberEight() {
-    let a = document.getElementById("results").value = +8;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +8;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +8;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function numberNine() {
-    let a = document.getElementById("results").value = +9;
-    document.getElementById("results").innerHTML = a;
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck == "0") {
+        let a = document.getElementById("results").value = "";
+        document.getElementById("results").innerHTML = a;
+        let b = document.getElementById("results").value = +9;
+        document.getElementById("results").innerHTML += b;
+    } else {
+        let b = document.getElementById("results").value = +9;
+        document.getElementById("results").innerHTML += b;
+    }
 }
 
 function clearButton() {
     let a = document.getElementById("results").value = 0;
     document.getElementById("results").innerHTML = a;
+}
+
+function numberFloat() {
+    let zeroCheck = document.getElementById("results").textContent;
+    if (zeroCheck.includes(".")) {
+        console.log("already floated")
+    } else {
+        let a = document.getElementById("results").value = ".";
+        document.getElementById("results").innerHTML += a;
+    }
 }
 
